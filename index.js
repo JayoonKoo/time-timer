34f8d1cd84a23747866c3a2fd6079114712d1016
JayoonKoo <rnwkdbs12@gmail.com>
2021-10-06 15:09:38 +0900

    init: 프로젝트 구성

---
+++ index.js
@@ -0,0 +1,39 @@
+const canvas = document.getElementById('main__timer-canvas');
+const ctx = canvas.getContext('2d');
+
+class Timer {
+	
+	constructor(time=3000) {
+		this._targetTime=time;
+		this._min = Number(time/60);
+		this._sec = time%60;
+	}
+
+	set targetTime(time) {
+		this._targetTime = time;
+	}
+
+	start = () => {
+		const startId = setInterval(() => {
+			
+		},1000);
+	}
+
+	get time() {
+		return {
+			min: this._min,
+			sec: this._sec,
+		}
+	}
+}
+
+
+const drayCtx = (color="rgb(243, 43, 61)") => {
+	ctx.fillStyle= color;
+	ctx.arc(50, 50, 20, 0, Math.PI * 2);
+	ctx.fill();
+}
+
+
+
+drayCtx();
