c492e7f0ca58b0c29baf0e4e69673fce7b4976b0
JayoonKoo <rnwkdbs12@gmail.com>
2021-10-08 13:00:05 +0900

    feat: timer digital 로 출력 기능 완료
    - todo :
    버튼 이벤트 연결 하기
    
    페이지 그리기

--- index.js
+++ index.js
@@ -1,39 +1,67 @@
 const canvas = document.getElementById('main__timer-canvas');
 const ctx = canvas.getContext('2d');
+const digitMin = document.querySelector('.main__timer-digit--min');
+const digitSec = document.querySelector('.main__timer-digit--sec');
+
+const DEFAULT_TIME = 3000;
+const CENTER_X = 350;
+const CENTER_Y = 300;
+const DEFAULT_COLOR ="rgb(243, 43, 61)";
+const DEFAULT_RADIUS = 300;
+
+const renderDigit = (time) => {
+	const {min, sec} = time;
+	digitMin.textContent = String(min).padStart(2, '0');
+	digitSec.textContent = String(sec).padStart(2, '0');
+}
 
 class Timer {
 	
-	constructor(time=3000) {
+	constructor(time=DEFAULT_TIME) {
 		this._targetTime=time;
-		this._min = Number(time/60);
+		this._currentTime = this._targetTime;
+		this._min = Math.floor(time/60);
 		this._sec = time%60;
+		this._startId;
 	}
 
 	set targetTime(time) {
 		this._targetTime = time;
 	}
 
-	start = () => {
-		const startId = setInterval(() => {
-			
-		},1000);
-	}
-
 	get time() {
+		this._min = Math.floor(this._currentTime/60);
+		this._sec = this._currentTime % 60;
 		return {
 			min: this._min,
 			sec: this._sec,
 		}
 	}
+
+	start = (renderDigit) => {
+		this._startId = setInterval(() => {
+			this._currentTime -= 1;
+			if (this._currentTime <=0) clearInterval(startId);
+			
+			const time = this.time;
+			renderDigit(time);
+		},1000);
+	}
+
+	finish = () => {
+		clearInterval(this._startId);
+	}
 }
 
+const timer = new Timer();
+timer.start(renderDigit);
+setTimeout(timer.finish, 70000);
 
-const drayCtx = (color="rgb(243, 43, 61)") => {
+
+const drayCtx = (color=DEFAULT_COLOR) => {
 	ctx.fillStyle= color;
-	ctx.arc(350, 300, 300, 0, Math.PI * 2);
+	ctx.arc(CENTER_X, CENTER_Y, DEFAULT_RADIUS, 0, Math.PI * 2);
 	ctx.fill();
 }
-
-
 
 drayCtx();
