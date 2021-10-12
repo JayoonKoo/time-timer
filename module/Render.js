76ca0647756db473d527b533c251c128ac600282
JayoonKoo <rnwkdbs12@gmail.com>
2021-10-12 16:32:06 +0900

    feat: drayDigit 추가
    캔버스 안에 디지털 숫자 추가

--- module/Render.js
+++ module/Render.js
@@ -2,30 +2,29 @@ const CANVAS_W = 700;
 const CANVAS_H = 600;
 const DEFAULT_COLOR = "rgb(243, 43, 61)";
 const DEFAULT_RADIUS = 300;
+const FONT = "bold 100px Arial";
+const BASELINE = "middle";
+const ALIGN = "center";
 
 const canvas = document.getElementById("main__timer-canvas");
 canvas.width = CANVAS_W;
 canvas.height = CANVAS_H;
 
 const ctx = canvas.getContext("2d");
+ctx.font = FONT;
+ctx.textBaseline = BASELINE;
+ctx.textAlign = ALIGN;
 
 const digitMin = document.querySelector(".main__timer-digit--min");
 const digitSec = document.querySelector(".main__timer-digit--sec");
 
 class Render {
-  constructor(
-		startRadian,
-    canvasW = CANVAS_W,
-    cnavasH = CANVAS_H,
-    radius = DEFAULT_RADIUS,
-    color = DEFAULT_COLOR,
-  ) {
-    this.canvasW = canvasW;
-    this.cnavasH = cnavasH;
-    this.radius = radius;
-    this.color = color;
-		this._startRadian = startRadian;
+  constructor(startRadian, time) {
+    this._startRadian = startRadian;
+		this._centerX = 0;
+		this._centerY = 0;
     this.drayCtx();
+		this.drayDigit(time);
   }
 
   renderDigit = (time) => {
@@ -34,14 +33,21 @@ class Render {
     digitSec.textContent = String(sec).padStart(2, "0");
   };
 
-  drayCtx = (startRadian=this._startRadian) => {
-    const centerX = this.canvasW / 2;
-    const centerY = this.cnavasH / 2;
-    ctx.fillStyle = this.color;
-		ctx.clearRect(0, 0, this.canvasW, this.cnavasH);
+  drayCtx = (startRadian = this._startRadian) => {
+    this._centerX = CANVAS_W / 2;
+    this._centerY = CANVAS_H / 2;
+    ctx.fillStyle = DEFAULT_COLOR;
+    ctx.clearRect(0, 0, CANVAS_W, CANVAS_H);
     ctx.beginPath();
-    ctx.moveTo(centerX, centerY);
-    ctx.arc(centerX, centerY, this.radius, Math.PI * (startRadian -0.5), Math.PI * 1.5, true);
+    ctx.moveTo(this._centerX, this._centerY);
+    ctx.arc(
+      this._centerX,
+      this._centerY,
+      DEFAULT_RADIUS,
+      Math.PI * (startRadian - 0.5),
+      Math.PI * 1.5,
+      true
+    );
     ctx.closePath();
     ctx.fill();
   };
@@ -52,6 +58,14 @@ class Render {
     } else {
       eventTarget.textContent = "Start";
     }
+  };
+
+  drayDigit = (time) => {
+		ctx.fillStyle = "#333";
+    let { min, sec } = time;
+    min = String(min).padStart(2, "0");
+    sec = String(sec).padStart(2, "0");
+    ctx.fillText(`${min} : ${sec}`, this._centerX, this._centerY);
   };
 }
 
