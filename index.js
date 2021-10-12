27e808da4d5f1e27f355d4fa7fa09d87466145ba
JayoonKoo <rnwkdbs12@gmail.com>
2021-10-12 14:57:28 +0900

    feat: timer canvas 움직임 구현

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ import Timer from './module/Timer.js';
 const resetBtn =  document.querySelector(".header__menu-item--reset-btn"),
 	startBtn = document.querySelector('.header__menu-item--start-btn');
 
-const timer = new Timer();
+const timer = new Timer(2700, startBtn);
 
 startBtn.addEventListener('click', timer.handelStart);
 resetBtn.addEventListener('click', timer.handelReset);
--- module/Timer.js
+++ module/Timer.js
@@ -2,15 +2,25 @@ import Render from './Render.js';
 
 const DEFAULT_TIME_SEC = 3000;
 
-const render = new Render();
 
 class Timer {
-	constructor(time=DEFAULT_TIME_SEC) {
+	constructor(time=DEFAULT_TIME_SEC, startBtn) {
 		this._targetTime=time;
+		this._resetStartRadian = this.getStartRadian(this._targetTime);
+		this._render = new Render(this._resetStartRadian);
 		this._currentTime = this._targetTime;
 		this._min = Math.floor(time/60);
 		this._sec = time%60;
 		this._startId;
+		this._startBtn = startBtn;
+	}
+
+	getStartRadian = (timeSec) => {
+		return (timeSec / 60 * 6 ) / 180;
+	}
+
+	get currentStartRadian() {
+		return this.getStartRadian(this._currentTime);
 	}
 
 	set targetTime(time) {
@@ -35,22 +45,22 @@ class Timer {
 			this.stop();
 		}
 		this._currentTime = this._targetTime;
-		render.renderDigit(this.time);
+		this._render.renderDigit(this.time);
 	}
 
 	handelStart = (event) => {
 		if (!this.isStarting) {
-			this.start(render.renderDigit);
+			this.start(() => this._render.drayCtx(this.currentStartRadian));
 		} else {
 			this.stop();
 		}
-		render.toggleStartStop(this.isStarting, event.target);
+		this._render.toggleStartStop(this.isStarting, this._startBtn);
 	}
 
 	start = (renderDigit) => {
 		this._startId = setInterval(() => {
 			this._currentTime -= 1;
-			if (this._currentTime < 0) clearInterval(startId);
+			if (this._currentTime < 0) clearInterval(this._startId);
 
 			const time = this.time;
 			renderDigit(time);
@@ -60,10 +70,7 @@ class Timer {
 	stop = () => {
 		clearInterval(this._startId);
 		this._startId = undefined;
-	}
-
-	finish = () => {
-		clearInterval(this._startId);
+		this._render.toggleStartStop(this.isStarting, this._startBtn);
 	}
 }
 
