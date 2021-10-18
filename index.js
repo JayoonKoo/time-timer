cfc3eb3d53c3f7125b235b0164f83ff7bec2cffb
JayoonKoo <rnwkdbs12@gmail.com>
2021-10-18 15:25:32 +0900

    feat: button 이벤트 추가

--- index.js
+++ index.js
@@ -2,7 +2,13 @@ import Timer from './module/Timer.js';
 
 const resetBtn =  document.querySelector(".header__menu-item--reset-btn"),
 	startBtn = document.querySelector('.header__menu-item--start-btn'),
-	settingBtn = document.querySelector('.header__menu-item--setting-btn');
+	settingBtn = document.querySelector('.header__menu-item--setting-btn'),
+	// modal Div 
+	modalDivMin = document.querySelector('.modal__time-minute'),
+	modalDivSec = document.querySelector('.modal__time-sec'),
+	// modal Button
+	modalButtons = document.querySelector('.modal__button');
+
 
 
 const timer = new Timer(2700, startBtn);
@@ -10,4 +16,8 @@ const timer = new Timer(2700, startBtn);
 startBtn.addEventListener('click', timer.handelStart);
 resetBtn.addEventListener('click', timer.handelReset);
 settingBtn.addEventListener('click', timer.handleSetting);
+
+modalDivMin.addEventListener('click', timer.handleMin);
+modalDivSec.addEventListener('click', timer.handleSec);
+modalButtons.addEventListener('click', timer.handleModalBtn);
 
