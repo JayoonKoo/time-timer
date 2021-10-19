7534b9fe3a8809278bc058620240cc75c2f48aee
JayoonKoo <rnwkdbs12@gmail.com>
2021-10-19 13:53:28 +0900

    feat: 입력 제한 사항 추가
    TO DO :
    1. 반응형 스타일
    2. modal 스타일 추가

--- module/Timer.js
+++ module/Timer.js
@@ -54,6 +54,12 @@ class Timer {
 		const {target: {value}} = event;
 		const numValue = Number(value);
 		const {target: {dataset: {type}}} = event;
+
+		if (isNaN(numValue)) {
+			alert("숫자를 입력해야 합니다.");
+			event.target.value = "0";
+			return;
+		}
 		switch (type) {
 			case "min":
 				if (numValue >= 60) {
@@ -64,6 +70,7 @@ class Timer {
 						alert("0보다 작은 값은 입력할 수 없습니다.");
 						event.target.value = "0";
 					} 
+					event.target.value = String(numValue);
 				}		
 				break;
 
@@ -76,6 +83,7 @@ class Timer {
 						alert("0보다 작은 값은 입력할 수 없습니다.");
 						event.target.value = "0";
 					} 
+					event.target.value = String(numValue);
 				}		
 				break;
 		
